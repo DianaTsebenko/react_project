2f30376b619fd297f83ba753532ed06e69aad7e6
MrScreamer <nostrees@ukr.net>
2025-01-02 03:43:08 +0100

    initial code added with gallery

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import MovieGallery from "./MovieGallery";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <MovieGallery />
+    </div>
+  );
+};
+
+export default App;
